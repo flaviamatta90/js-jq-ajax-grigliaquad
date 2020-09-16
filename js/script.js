dc25c4ec4238d91ce3ccfed44a279475195e1413
Flavia <flaviamatta90@gmail.com>
2020-09-16 16:30:41 +0200

    change background-color

--- js/script.js
+++ js/script.js
@@ -6,30 +6,28 @@ $(document).ready(
     // Se è <= 5 il quadrato diventa giallo, se è > di 5 il quadrato diventa verde.
     // Il numero ottenuto appare al centro del quadrato.
 
-    $.ajax(
-    {
-      url: "https://flynn.boolean.careers/exercises/api/random/int",
-      "method": "GET",
-      "success": function (data, stato) {
-        var rispostaServer = data.response;
-        stampaRisultato(rispostaServer);
-        console.log(data.response);
-      },
-      error: function (richiesta, stato, errori) {
-      alert("E' avvenuto un errore. " + errore);
-      }
-    }
-    );
+    $(".box").click(function(){
 
-    function stampaRisultato (numServer){
-      console.log(numServer)
-      if (numServer <= 5){
-        alert("ha vinto il pc");
-      } else{
-        alert("hai vinto tu");
-      };
-    };
+      var clickBox = $(this);
 
+      $.ajax(
+      {
+        url: "https://flynn.boolean.careers/exercises/api/random/int",
+        "method": "GET",
+        "success": function (data, stato) {
+          var rispostaServer = data.response;
+          console.log(data.response);
 
+          if (rispostaServer <= 5){
+            clickBox.addClass("yellow");
+          }else{
+            clickBox.addClass("green");
+          }
+
+        }
+
+      });
+
+    });
 
   });
