e8332765223f60c69ea21ee43f6fe1eb1b1916c3
Flavia <flaviamatta90@gmail.com>
2020-09-16 16:51:31 +0200

    error

--- js/script.js
+++ js/script.js
@@ -3,9 +3,6 @@ $(document).ready(
   function (){
 
     // Generare una griglia 6x6 (36 boxes), ad ogni click parte una richiesta AJAX che prende un numero random da 1 a 9 (scegliere API opportuna).
-    // Se è <= 5 il quadrato diventa giallo, se è > di 5 il quadrato diventa verde.
-    // Il numero ottenuto appare al centro del quadrato.
-
     $(".box").click(function(){
 
       var clickBox = $(this);
@@ -18,13 +15,19 @@ $(document).ready(
           var rispostaServer = data.response;
           console.log(data.response);
 
+        // Se è <= 5 il quadrato diventa giallo, se è > di 5 il quadrato diventa verde.
           if (rispostaServer <= 5){
             clickBox.addClass("yellow");
           }else{
             clickBox.addClass("green");
           }
 
+        // Il numero ottenuto appare al centro del quadrato.
           clickBox.text(rispostaServer);
+        },
+
+        "error": function (richiesta, stato, errori) {
+          alert("E' avvenuto un errore. " + errore);
         }
 
       });
